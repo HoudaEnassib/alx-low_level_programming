d6abf649f553cfe623ddb75376bbb3261af0afd7
HoudaEnassib <hnour6525@gmail.com>
2023-10-08 08:06:21 +0000

    all

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
