af047cc23f35143439a1003611c28dddb03673f2
HoudaEnassib <hnour6525@gmail.com>
2023-10-08 13:25:24 +0000

    all

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: multi of two numbers from agruments
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	return (0);
+}
