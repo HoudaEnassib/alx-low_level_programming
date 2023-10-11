6cd8efec4a20d72a160a2313a662e7c7dc4d39b3
HoudaEnassib <hnour6525@gmail.com>
2023-10-11 01:22:50 +0000

    new commit

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -2,11 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - a dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: dog owner's name
- * Description: dog struct containing the name, age and owner's name.
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
  */
 struct dog
 {
@@ -15,13 +17,11 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for dog
- */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
